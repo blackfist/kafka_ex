a0298d80842c1cbe1239dbbcd283fee2ebd6a918
Kevin Thompson <kevin.david.thompson@gmail.com>
2016-06-13 15:59:37 -0500

    making improvements to the create_request function

--- lib/kafka_ex/protocol/describe_group.ex
+++ lib/kafka_ex/protocol/describe_group.ex
@@ -1,14 +1,16 @@
 defmodule KafkaEx.Protocol.DescribeGroup do
+  # described here: https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol#AGuideToTheKafkaProtocol-DescribeGroupsRequest
   import KafkaEx.Protocol.Common
 
   defmodule Response do
+    # described here: https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol#AGuideToTheKafkaProtocol-DescribeGroupsResponse
     defstruct error_code: nil,
       group_id: nil,
       state: nil,
       protocol_type: nil,
       protocol: nil,
       members: []
-      
+
     @type t :: %Response{error_code: atom | integer,
       group_id: binary,
       state: binary,
@@ -18,7 +20,8 @@ defmodule KafkaEx.Protocol.DescribeGroup do
   end
 
   @spec create_request(binary) :: binary
-  def create_request(group_name) do
+  def create_request(correlation_id, client_id, group_name) do
+    KafkaEx.Protocol.create_request(:describe_group, correlation_id, client_id) <>
     << byte_size(group_name) :: 16-signed, group_name :: binary >>
   end
 end
